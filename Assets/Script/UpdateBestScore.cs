1ecc7ea2766878ba7eb84865e7d821751dcc442c
ghdwl <ghdwls4654@naver.com>
2024-04-16 17:45:17 +0900

    스테이지 해금 조건 추가
    이전 난이도에서 특정 점수 이상 획득 시 다음 난이도 해금.

--- Assets/Script/UpdateBestScore.cs
+++ Assets/Script/UpdateBestScore.cs
@@ -9,11 +9,47 @@ public class UpdateBestScore : MonoBehaviour
     public Text BestScore_1;
     public Text BestScore_2;
 
+    public GameObject NormalStage;
+    public GameObject NormalStageInactive;
+    public GameObject HardStage;
+    public GameObject HardStageInactive;
+
+    [SerializeField] private int UnlockNormalStage = 2000;
+    [SerializeField] private int UnlockHardStage = 2000;
+
     // Start is called before the first frame update
     void Start()
     {
+        ActiveStages();
         BestScore_0.text = PlayerPrefs.GetInt("BestRecord0").ToString();
         BestScore_1.text = PlayerPrefs.GetInt("BestRecord1").ToString();
         BestScore_2.text = PlayerPrefs.GetInt("BestRecord2").ToString();
+    }
+    void ActiveStages()
+    {
+        int BestScore_0 = PlayerPrefs.GetInt("BestRecord0");
+        int BestScore_1 = PlayerPrefs.GetInt("BestRecord1");
+
+        if(BestScore_0 >= UnlockNormalStage)
+        {
+            NormalStage.SetActive(true);
+            NormalStageInactive.SetActive(false);
+        }
+        else
+        {
+            NormalStage.SetActive(false);
+            NormalStageInactive.SetActive(true);
+        }
+
+        if (BestScore_1 >= UnlockHardStage)
+        {
+           HardStage.SetActive(true);
+           HardStageInactive.SetActive(false);
+        }
+        else
+        {
+            HardStage.SetActive(false);
+            HardStageInactive.SetActive(true);
+        }
     }
 }
